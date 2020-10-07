9f44c69dec2f4faa6187df5860386980cd643c6e
J V S PRAKASH UPPALA <prakash.ujvs@ltts.com>
2020-10-07 23:22:55 +0530

    firdst

---
+++ inc/function.h
@@ -0,0 +1,13 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <stdio.h>
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void binary();
+void octal();
+void calculator_operations();
+
+#endif
---
+++ src/function.c
@@ -0,0 +1,98 @@
+#include<function.h>
+#include<stdio.h>
+#include<math.h>
+ void calculator_operations()
+{
+    //system("cls");  use system function to clear 
+    //screen instead of clrscr();
+    printf("\n             Welcome to C calculator \n\n");
+ 
+    printf("******* Press 'Q' or 'q' to quit ");
+    printf("the program ********\n");
+    printf("***** Press 'H' or 'h' to display ");
+    printf("below options *****\n\n");
+    printf("Enter 'C' or 'c' to clear the screen and");
+    printf(" display available option \n\n");
+ 
+    printf("Enter + symbol for Addition \n");
+    printf("Enter - symbol for Subtraction \n");
+    printf("Enter * symbol for Multiplication \n");
+    printf("Enter / symbol for Division \n");
+    printf("Enter b symbol for to convert Binary\n");
+    printf("Enter o symbol for to convert octl \n\n");
+}
+ 
+void addition()
+{
+    int n, total=0, k=0, number;
+    printf("\nEnter the number of elements you want to add:");
+    scanf("%d",&n);
+    printf("Please enter %d numbers one by one: \n",n);
+    while(k<n)
+    { 
+        scanf("%d",&number);
+        total=total+number;
+        k=k+1;
+    }
+    printf("Sum of %d numbers = %d \n",n,total);
+}
+ 
+void subtraction()
+{ 
+    int a, b, c = 0; 
+    printf("\nPlease enter first number  : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number : "); 
+    scanf("%d", &b); 
+    c = a - b; 
+    printf("\n%d - %d = %d\n", a, b, c); 
+}
+ 
+void multiplication()
+{
+    int a, b, mul=0; 
+    printf("\nPlease enter first numb   : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number: "); 
+    scanf("%d", &b);
+    mul=a*b;
+    printf("\nMultiplication of entered numbers = %d\n",mul);
+}
+ 
+void division()
+{
+    int a, b, d=0; 
+    printf("\nPlease enter first number  : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number : "); 
+    scanf("%d", &b);
+    d=a/b;
+    printf("\nDivision of entered numbers=%d\n",d);
+}
+ 
+int octal(int decimalNumber)
+{
+    int octalNumber = 0, i = 1;
+
+    while (decimalNumber != 0)
+    {
+        octalNumber += (decimalNumber % 8) * i;
+        decimalNumber /= 8;
+        i *= 10;
+    }
+
+    return octalNumber;
+} 
+int binary(int decimalNumber)
+{
+    int binaryNumber = 0, i = 1;
+
+    while (decimalNumber != 0)
+    {
+        binaryNumber += (decimalNumber % 2) * i;
+        decimalNumber /= 2;
+        i *= 10;
+    }
+
+    return binaryNumber;
+} 
